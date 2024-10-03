6c1bc0705d5b30b9af8fda4a79b78fd461c13a2f
Matthew East <matthew_east@apple.com>
2024-10-03 17:36:19 +0100

    FRONT END IS WORKING

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
+
+import downloads
 
 app = Flask(__name__, static_folder='static')
 
@@ -6,6 +8,11 @@ app = Flask(__name__, static_folder='static')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.post("/organise")
+def organise():
+    downloads.download_organiser()
+    return redirect(request.referrer)
 
 if __name__ == '__main__':
     app.run(debug=True)
