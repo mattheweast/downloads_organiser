9f3e80701c68026ca44fc2734e08c7963811baad
Matthew East <matthew_east@apple.com>
2024-10-03 16:26:05 +0100

    Updated HTML

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
-
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static')
