bec57fd7591f322ebd7ea5be091c460e1e98fcbe
Matthew East <contact@matteast.uk>
2024-10-08 15:36:01 +0100

    Updated categories, trying yml

--- downloads.py
+++ downloads.py
@@ -1,21 +1,20 @@
 import os
 import shutil
+import yaml
 from pathlib import Path
 
-def download_organiser():
-# File types categorised by extensions
-    categories = {
-        "Audio": ['.aif', '.cda', '.mid', '.midi', '.mp3', '.mpa', '.ogg', '.wav', '.wma'],
-        "Compressed": ['.7z', '.deb', '.pkg', '.rar', '.rpm', '.tar.gz', '.z', '.zip'],
-        "Code": ['.js', '.jsp', '.html', '.ipynb', '.py', '.java', '.css'],
-        "Documents": ['.pdf', '.xls', '.xlsx', '.doc', '.docx', '.txt'],
-        "Images": ['.bmp', '.gif', '.ico', '.jpeg', '.jpg', '.png', '.jfif', '.svg', '.tif', '.tiff'],
-        "Software": ['.apk', '.bat', '.bin', '.exe', '.jar', '.msi', '.py'],
-        "Videos": ['.3gp', '.avi', '.flv', '.h264', '.mkv', '.mov', '.mp4', '.mpg', '.mpeg', '.wmv'],
-        "Other": []
+categories = {
+    "Audio": ['.aif', '.cda', '.mid', '.midi', '.mp3', '.mpa', '.ogg', '.wav', '.wma'],
+    "Compressed": ['.7z', '.deb', '.pkg', '.rar', '.rpm', '.tar.gz', '.z', '.zip'],
+    "Code": ['.js', '.jsp', '.html', '.ipynb', '.py', '.java', '.css'],
+    "Documents": ['.pdf', '.xls', '.xlsx', '.doc', '.docx', '.txt'],
+    "Images": ['.bmp', '.gif', '.ico', '.jpeg', '.jpg', '.png', '.jfif', '.svg', '.tif', '.tiff'],
+    "Software": ['.apk', '.bat', '.bin', '.exe', '.jar', '.msi', '.py'],
+    "Videos": ['.3gp', '.avi', '.flv', '.h264', '.mkv', '.mov', '.mp4', '.mpg', '.mpeg', '.wmv'],
+    "Other": []
     }
 
-
+def download_organiser():
     # Assigning the root download folder
     home_dir = Path.home()
     download_root = home_dir / "Downloads"
